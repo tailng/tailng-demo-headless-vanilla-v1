580140b46e0caa00054c2ec77025bb10fdd555ef
Prince Francis <princekfrancis@gmail.com>
2026-04-01 11:59:14 +0530

    switch examples added.

--- src/app/playground/form/switch/switch.component.ts
+++ src/app/playground/form/switch/switch.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TngSwitch } from '@tailng-ui/primitives';
 
 @Component({
   selector: 'app-switch',
   standalone: true,
   templateUrl: './switch.component.html',
   styleUrl: './switch.component.css',
+  imports: [TngSwitch],
 })
-export class SwitchComponent {}
+export class SwitchComponent {
+  readonly releaseReady = signal(true);
+
+  onReleaseReadyToggle(): void {
+    this.releaseReady.update((value) => !value);
+  }
+}
